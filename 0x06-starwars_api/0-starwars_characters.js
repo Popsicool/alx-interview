79a808d857a6b744cc43a7aa122fd7f639bfe54e
Popsicool <akinolasamson1234@gmail.com>
2023-05-30 15:53:00 +0100

    fix semistandard

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -2,20 +2,20 @@
 
 // Star Wars Characters
 const request = require('request');
-const num = process.argv[2]
-const url = `https://swapi-api.alx-tools.com/api/films/${num}`
-const url2 = "https://swapi-api.alx-tools.com/api/people/"
+const num = process.argv[2];
+const url = `https://swapi-api.alx-tools.com/api/films/${num}`;
+const url2 = 'https://swapi-api.alx-tools.com/api/people/';
 
-request(url, function(error, response, body){
-    const char = JSON.parse(body)["characters"]
-    char.forEach(element => {
-        const el = parseInt(element.split("/").slice(-2, -1).toString())
-        request(`${url2 + el}`, function(error2, response2, body2){
-            const name = JSON.parse(body2)["name"]
-            console.log(name)
-        })
+request(url, function (error, response, body) {
+  if (error) {
+    console.log(error);
+  }
+  const char = JSON.parse(body).characters;
+  char.forEach(element => {
+    const el = parseInt(element.split('/').slice(-2, -1).toString());
+    request(`${url2 + el}`, function (error2, response2, body2) {
+      const name = JSON.parse(body2).name;
+      console.log(name);
     });
-})
-
-function getUser(url){
-}
+  });
+});
