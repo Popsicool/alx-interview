88109de6f1b0f1adfaee0a987a878d34261346b3
Popsicool <akinolasamson1234@gmail.com>
2023-06-21 22:47:17 +0100

    fix bug

--- 0x09-island_perimeter/0-island_perimeter.py
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -11,7 +11,7 @@ def island_perimeter(grid: List[List[int]]) -> int:
     '''
     per = 0
     for i in range(len(grid)):
-        for j in range(len(grid)):
+        for j in range(len(grid[0])):
             if grid[i][j]:
                 if not grid[i - 1][j]:
                     per += 1
