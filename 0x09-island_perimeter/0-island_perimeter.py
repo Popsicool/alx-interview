db9ce48ca9052e0f9632ec60de13e6e0e13aeaf3
Popsicool <akinolasamson1234@gmail.com>
2023-06-21 22:53:33 +0100

    removed typing

--- 0x09-island_perimeter/0-island_perimeter.py
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -2,43 +2,12 @@
 '''
 island parameter
 '''
-from typing import List
 
 
-def island_perimeter(grid: List[List[int]]) -> int:
+def island_perimeter(grid):
     '''
     island parameter
     '''
-    H = len(grid)
-    W = len(grid[0])
-
-    def depthFirst(r: int, c: int) -> int:
-        if r < 0 or r >= H or c < 0 or c >= W or grid[r][c] == 0:
-            return 1
-        if grid[r][c] == 1:
-            grid[r][c] = 2
-            return depthFirst(r-1, c) + depthFirst(r+1, c) + depthFirst(
-                r, c+1) + depthFirst(r, c-1)
-    perimeter = 0
-    for row in range(H):
-        for col in range(W):
-            if grid[row][col] == 1:
-                perimeter += depthFirst(row, col)
-    return perimeter
-
-
-
-'''
-
-island parameter
-
-from typing import List
-
-
-def island_perimeter(grid: List[List[int]]) -> int:
-
-    island parameter
-
     per = 0
     for i in range(len(grid)):
         for j in range(len(grid[0])):
@@ -52,5 +21,3 @@ def island_perimeter(grid: List[List[int]]) -> int:
                 if not grid[i][j - 1]:
                     per += 1
     return per
-
-'''
