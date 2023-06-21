f3c04242550dfd2829f4119a1ce41ee2891b425c
Popsicool <akinolasamson1234@gmail.com>
2023-06-21 22:41:36 +0100

    changed to interation

--- 0x09-island_perimeter/0-island_perimeter.py
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -1,44 +1,24 @@
 #!/usr/bin/python3
 '''
-is island perimeter
+island parameter
 '''
+from typing import List
 
-def island_perimeter(grid):
-	'''
-	is island
-	'''
-	new_grid = grid[1:]
-	pos = []
-	for idx, row in enumerate(new_grid):
-		state = False
-		for idx2, col in enumerate(row):
-			if col:
-				pos = [idx + 1, idx2]
-				state = True
-				break
-		if state:
-			break
-	if len(pos) < 1:
-		return 0
-	if not grid[pos[0]][pos[1] + 1]:
-		height = 0
-		h = pos[0]
-		while grid[h][pos[1]]:
-			h += 1
-			height += 1
-		width = 0
-		for i in grid[height + pos[0] - 1]:
-			if i:
-				width += 1
-	else:
-		width = 0
-		w = pos[1]
-		while grid[pos[0]][w]:
-			w += 1
-			width += 1
-		height = 0
-		h = pos[0]
-		while grid[h][width]:
-			h += 1
-			height += 1
-	return 2 * (height + width)
+
+def island_perimeter(grid: List[List[int]]) -> int:
+    '''
+    island parameter
+    '''
+    per = 0
+    for i in range(len(grid)):
+        for j in range(len(grid)):
+            if grid[i][j]:
+                if not grid[i - 1][j]:
+                    per += 1
+                if not grid[i][j + 1]:
+                    per += 1
+                if not grid[i + 1][j]:
+                    per += 1
+                if not grid[i][j - 1]:
+                    per += 1
+    return per
